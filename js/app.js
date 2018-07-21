ed29bc384e6471151b205569a6a17900244921d9
Inna Yarantseva <innayarantseva@gmail.com>
2018-07-21 22:56:58 +0300

    add basic functionality

--- js/app.js
+++ js/app.js
@@ -1,38 +1,169 @@
-/*
- * Create a list that holds all of your cards
- */
+const deck             = document.querySelector( '.deck'  ),
+      timerPlaceholder = document.querySelector( '.timer' ),
+      movesPlaceholder = document.querySelector( '.moves' );
+
+let moves           = 0,
+    pairs           = 0,
+    gameTimer       = false, 
+    gameTimerActive = false,
+    tries           = [],
+    currentlyOpen   = [];
+
+const symbolClasses = [
+    'fa-diamond', 
+    'fa-diamond', 
+    'fa-paper-plane-o', 
+    'fa-paper-plane-o', 
+    'fa-anchor', 
+    'fa-anchor', 
+    'fa-bolt', 
+    'fa-bolt', 
+    'fa-cube', 
+    'fa-cube', 
+    'fa-leaf', 
+    'fa-leaf', 
+    'fa-bicycle',
+    'fa-bicycle',
+    'fa-bomb', 
+    'fa-bomb'
+];
+
+/* State of the Game Functions */
+
+const startTimer = () => {
+        let min = 0,
+            sec = 1;
+
+        gameTimerActive = true;
+
+        gameTimer = setInterval(
+            () => {
+                sec < 10 
+                    ? timerPlaceholder.innerHTML = `${min}:0${sec}`
+                    : timerPlaceholder.innerHTML = `${min}:${sec}`;
+
+                if (sec === 59) {
+                    min++;
+                    sec = 0;
+                } else {
+                    sec++;
+                }
+
+            }, 
+            1000
+        );
+
+    },
+
+    setCounter = () => movesPlaceholder.textContent = moves;
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+/* Interaction with Deck Functions */
+
+var resetDeck = () => {
+        while ( deck.firstChild ) { 
+            deck.removeChild( deck.firstChild ); 
+        }
+    },
+
+    generateCards = () => {
+        const fragment = document.createDocumentFragment();
+
+        while ( fragment.childElementCount < 16 ) {
+            const card       = document.createElement( 'li' );
+            const cardSymbol = document.createElement( 'i'  );
+
+            card      .classList.add( 'card' );
+            cardSymbol.classList.add( 'fa'   );
+
+            card.append( cardSymbol );
+            fragment.appendChild( card );
+        }
+
+        deck.append(fragment);
+    },
+
+    shuffleDeck = () => {
+        const cardsCollection = deck.getElementsByClassName('fa'),
+              shuffledSymbolClasses = shuffle( symbolClasses );
+
+        shuffledSymbolClasses.forEach( 
+            (symbolClass, index) => 
+                cardsCollection[index].classList.add( symbolClass ) 
+        );
+    },
+
+    handleCardClick = ({target}) => {
+        !gameTimerActive && startTimer();
+
+        if ( target.nodeName === 'LI' && !isOpened( target ) ) {
+            showSymbol( target );
+            checkForMatch();
+        }
+    },
+
+    isOpened = card => card.classList.contains( 'open' ),
+
+    showSymbol = card => {
+        var cardClasses = card.classList;
+
+        cardClasses.add( 'open', 'show' );
+        currentlyOpen.push( card );
+    },
+
+    checkForMatch = () => {
+        if ( currentlyOpen.length === 2 ) {
+            const a = currentlyOpen[0];
+            const b = currentlyOpen[1];
+
+            deck.removeEventListener( 'click', handleCardClick );
+
+            if ( a.innerHTML === b.innerHTML ) {
+                a.classList.add( 'match' );
+                b.classList.add( 'match' );
+                pairs++;
+            }
+
+            setTimeout( 
+                () => {
+                    a.classList.remove( 'open', 'show' );
+                    b.classList.remove( 'open', 'show' );
+
+                    currentlyOpen = [];
+
+                    deck.addEventListener( 'click', handleCardClick );
+                }, 
+                500
+            );
+
+            moves++;
+            setCounter();
+        }
+    }
+
+    setNewGame = () => {
+        resetDeck();
+        generateCards();
+        shuffleDeck();
+        deck.addEventListener( 'click', handleCardClick );
+    };
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length - 1, 
+        temporaryValue, 
+        randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
+        currentIndex -= 1;
     }
 
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+setNewGame();
